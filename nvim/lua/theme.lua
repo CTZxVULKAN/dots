dde2cd053a9b8d50bc45c84d391482428b370ae1
VULKAN <65299153+CTZxVULKAN@users.noreply.github.com>
2021-07-28 08:45:32 +0530

    Update theme.lua

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -1,9 +1,9 @@
-vim.g.nvchad_theme = "onedark"
+vim.g.nvchad_theme = "javacafe"
 
 local present, base16 = pcall(require, "base16")
 
 if present then
-    base16(base16.themes["onedark"], true)
+    base16(base16.themes["javacafe"], true)
     require "highlights"
     return true
 else
