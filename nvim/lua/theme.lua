b3aacacb67a6e7aabc18507eda4a9f05cfd92c31
VULKAN <vulkan.dev@protonmail.com>
2021-08-01 11:44:25 +0530

    colorscheme changes

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -1,9 +1,9 @@
-vim.g.nvchad_theme = "javacafe"
+vim.g.nvchad_theme = "onedark"
 
 local present, base16 = pcall(require, "base16")
 
 if present then
-    base16(base16.themes["javacafe"], true)
+    base16(base16.themes["onedark"], true)
     require "highlights"
     return true
 else
