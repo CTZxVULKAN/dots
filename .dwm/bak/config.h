2fbb71cb640dd06c930b1cf04842e73601ee3ab9
VULKAN <vulkan.dev@protonmail.com>
2021-09-22 15:59:49 +0530

    modified dwm

--- .dwm/bak/config.h
+++ .dwm/bak/config.h
@@ -7,6 +7,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 30;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int vertpad            = 6;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
