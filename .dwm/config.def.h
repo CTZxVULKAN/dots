a89ae9155a370f6f240a6094a218aa4b9fb5e27c
VULKAN <vulkan.dev@protonmail.com>
2021-09-24 22:09:56 +0530

    font size change

--- .dwm/config.def.h
+++ .dwm/config.def.h
@@ -9,7 +9,7 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 33;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const int vertpad            = 6;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
-static const char *fonts[]          = { "monospace:size=10","Hack Nerd Font Mono:size=16" };
+static const char *fonts[]          = { "Ubuntu Regular:size=10","Hack Nerd Font Mono:size=16"};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
